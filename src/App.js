84186212a25d43f0e0b50b73f954177f9c0e67d5
Mar'i Fauzan <marifauzan20@gmail.com>
2022-01-08 22:02:14 +0700

    Add routing to Project

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
         <Routes>
           <Route path="/" exact element={<Home />} />
           <Route path="/movie/:imdbID" element={<MovieDetail />} />
-          <Route element={<PageNotFound />} />
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
         <Footer />
       </BrowserRouter>
